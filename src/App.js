86db49be622396e8bbd77735a67ec1402c4a43e0
rafael.bonamigo <rafael.bonamigo@siagri.com.br>
2022-09-11 20:21:12 -0300

    HTML refactored rendering crew using map and refactored CSS

--- src/App.js
+++ src/App.js
@@ -6,6 +6,39 @@ import photo4 from './images/photo4.png'
 import photo5 from './images/photo5.png'
 import photo6 from './images/photo6.png'
 
+const crew = [
+  {
+    name: 'Bill Mahoney',
+    role: 'Product Owner',
+    avatar: photo1
+  },
+  {
+    name: 'Saba Cabrera',
+    role: 'Art Director',
+    avatar: photo2
+  },
+  {
+    name: 'Shae Le',
+    role: 'Tech Lead',
+    avatar: photo3
+  },
+  {
+    name: 'Skylah Lu',
+    role: 'UX Designer',
+    avatar: photo4
+  },
+  {
+    name: 'Griff Richard',
+    role: 'Developer',
+    avatar: photo5
+  },
+  {
+    name: 'Stan John',
+    role: 'Developer',
+    avatar: photo6
+  }
+]
+
 function App() {
   return (
     <div className={styles.container}>
@@ -17,36 +50,15 @@ function App() {
         </section>
       </header>
       <section className={styles.team}>
-        <div>
-          <img src={photo1} alt="bill mahoney" />
-          <p>Bill Mahoney</p>
-          <span id={styles.bill} >Product Owner</span>
-        </div>
-        <div>
-          <img src={photo2} alt="saba cabrera" />
-          <p>Saba Cabrera</p>
-          <span id={styles.saba} >Art Director</span>
-        </div>
-        <div>
-          <img src={photo3} alt="shae le" />
-          <p>Shae Le</p>
-          <span id={styles.shae} >Tech Lead</span>
-        </div>
-        <div>
-          <img src={photo4} alt="skylah lu" />
-          <p>Skylah Lu</p>
-          <span id={styles.skylah} >UX Designer</span>
-        </div>
-        <div>
-          <img src={photo5} alt="griff richards" />
-          <p>Griff Richards</p>
-          <span id={styles.griff} >Developer</span>
-        </div>
-        <div>
-          <img src={photo6} alt="stan john" />
-          <p>Stan John</p>
-          <span id={styles.stan} >Developer</span>
-        </div>
+        {crew.map(member => {
+          return (
+            <div>
+              <img src={member.avatar} alt={member.name} />
+              <p>{member.name}</p>
+              <span>{member.role}</span>
+            </div>
+          )
+        })}
       </section>
       <footer>
         created by <a href="https://github.com.br/bonamigor">bonamigor</a> - devChallenges.io
